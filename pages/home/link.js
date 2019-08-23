9435e8c021b45342af2971543ab94ac599d24f31
wuji2lili <3096747542@qq.com>
2019-08-23 10:53:42 +0800

    问卷

--- pages/home/link.js
+++ pages/home/link.js
@@ -1,23 +1,26 @@
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    extendUrl : ''
-  },
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        extendUrl: ''
+    },
 
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
+    /**
+     * 生命周期函数--监听页面加载
+     */
 
-  onLoad: function (options) {
-    this.setData({
-      extendUrl: options.extendUrl //这个options就是navigator跳转过来的url参数
-    }) 
-    console.log(this.data.extendUrl)    
-  },
+    onLoad: function(options) {
+        this.setData({
+                extendUrl: options.extendUrl //这个options就是navigator跳转过来的url参数
+            })
+            // this.setData({
+            //     extendUrl: 'http://192.168.0.101:8080/protocal?userCode=' + wx.getStorageSync('userCode') //这个options就是navigator跳转过来的url参数
+            // })
+        console.log(this.data.extendUrl)
+    },
 
 
 
