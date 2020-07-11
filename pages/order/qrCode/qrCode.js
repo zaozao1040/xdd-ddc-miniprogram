9addbbf2f170f1ce7b31316cf9e5780171ad77c3
邱宁 <zqiuning@163.com>
2020-07-11 15:51:15 +0800

    已完成 订单电子凭证 加入取餐按钮

--- pages/order/qrCode/qrCode.js
+++ pages/order/qrCode/qrCode.js
@@ -3,6 +3,13 @@ var qrcode;
 const W = wx.getSystemInfoSync().windowWidth;
 const rate = 750.0 / W;
 const code_w = 300 / rate;
+
+import {
+  base
+} from '../../../comm/public/request'
+
+let requestModel = new base()
+
 Page({
 
   /**
@@ -12,7 +19,8 @@ Page({
     userInfo: wx.getStorageSync("userInfo").userInfo,
     detailInfo: {},
 
-    code_w: code_w
+    code_w: code_w,
+    pickStatus: -1
   },
 
   /**
@@ -24,12 +32,65 @@ Page({
     var detailInfo = prevPage.data.detailInfo
     this.setData({
       detailInfo: detailInfo,
+    }, () => {
+      this.makeQrcode(detailInfo.orderCode)
     });
 
-    this.makeQrcode(detailInfo.orderCode)
+  },
 
+  // 获取订单取餐状态
+  getOrderPickStatus: function () {
+    let _this = this
+    let param = {
+      userCode: wx.getStorageSync('userCode'),
+      orderCode: _this.data.detailInfo.orderCode
+    }
+    let params = {
+      data: param,
+      url: '/order/getPickStatus',
+      method: 'post'
+    }
+    requestModel.request(params, (result) => {
+      _this.setData({
+        pickStatus: result.pickStatus,
+      });
+    })
+  },
+  // 修改订单取餐状态 从未取餐改为已取餐
+  updateOrderStatus: function () {
+    let _this = this
+    let param = {
+      userCode: wx.getStorageSync('userCode'),
+      orderCode: _this.data.detailInfo.orderCode
+    }
+    let params = {
+      data: param,
+      url: '/order/updatePickStatus',
+      method: 'post'
+    }
+    wx.showModal({
+      title: "是否取餐",
+      content: "请打餐时出示",
+      success: function (res) {
+        if (res.confirm) {
+          requestModel.request(params, (result) => {
+            wx.showToast({
+              title: '取餐成功',
+              duration: 3000
+            })
+            _this.getOrderPickStatus()
+          })
+        } else if (res.cancel) {
+          console.log('用户点击取消')
+        }
+
+      },
+    });
 
   },
+
+
+
   makeQrcode: function (text) {
     qrcode = new QRCode('canvas', {
       text: text,
@@ -49,7 +110,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    this.getOrderPickStatus()
   },
 
   /**
