b29a1f487daefb6da2b1af9bb1d88e510a932709
邱宁 <zqiuning@163.com>
2021-01-23 13:59:48 +0800

    电子凭证加入餐别

--- pages/order/qrCode/qrCode.js
+++ pages/order/qrCode/qrCode.js
@@ -1,17 +1,14 @@
-var QRCode = require('../../../comm/public/weapp-qrcode.js')
+var QRCode = require("../../../comm/public/weapp-qrcode.js");
 var qrcode;
 const W = wx.getSystemInfoSync().windowWidth;
 const rate = 750.0 / W;
 const code_w = 300 / rate;
 
-import {
-  base
-} from '../../../comm/public/request'
+import { base } from "../../../comm/public/request";
 
-let requestModel = new base()
+let requestModel = new base();
 
 Page({
-
   /**
    * 页面的初始数据
    */
@@ -21,7 +18,7 @@ Page({
 
     code_w: code_w,
     pickStatus: -1,
-    ningxiaOrgFlag: false
+    ningxiaOrgFlag: false,
   },
 
   /**
@@ -29,42 +26,58 @@ Page({
    */
 
   onLoad: function (options) {
-    let _this = this
-    let tmp_ningxiaOrgFlag = this.checkNingxia()
+    let _this = this;
+    let tmp_ningxiaOrgFlag = this.checkNingxia();
     if (tmp_ningxiaOrgFlag) {
       _this.setData({
-        ningxiaOrgFlag: tmp_ningxiaOrgFlag
+        ningxiaOrgFlag: tmp_ningxiaOrgFlag,
       });
     }
 
-    requestModel.getUserCode(userCode => {
+    requestModel.getUserCode((userCode) => {
       let param = {
-        url: '/order/getOrderDetail?userCode=' + userCode + '&orderCode=' + options.orderCode
-      }
-      requestModel.request(param, data => {
-        _this.setData({
-          detailInfo: data,
-          userInfo: wx.getStorageSync("userInfo").userInfo
-        }, () => {
-          this.getOrderPickStatus()
-          this.makeQrcode(data.orderCode)
-
-        });
-      })
-    })
+        url:
+          "/order/getOrderDetail?userCode=" +
+          userCode +
+          "&orderCode=" +
+          options.orderCode,
+      };
+      requestModel.request(param, (data) => {
+        if (data.mealType == "BREAKFAST") {
+          data.mealTypeDes = "早餐";
+        } else if (data.mealType == "LUNCH") {
+          data.mealTypeDes = "午餐";
+        } else if (data.mealType == "DINNER") {
+          data.mealTypeDes = "晚餐";
+        } else if (data.mealType == "NIGHT") {
+          data.mealTypeDes = "夜宵";
+        }
+        data.mealTypeDes;
+        _this.setData(
+          {
+            detailInfo: data,
+            userInfo: wx.getStorageSync("userInfo").userInfo,
+          },
+          () => {
+            this.getOrderPickStatus();
+            this.makeQrcode(data.orderCode);
+          }
+        );
+      });
+    });
   },
   // 判断是否是宁夏
   checkNingxia: function () {
-    let organizeCode = wx.getStorageSync("userInfo").userInfo.organizeCode
-    let ningxiaOrgCode = getApp().globalData.ningxiaOrgnaizeCode
+    let organizeCode = wx.getStorageSync("userInfo").userInfo.organizeCode;
+    let ningxiaOrgCode = getApp().globalData.ningxiaOrgnaizeCode;
     if (organizeCode === ningxiaOrgCode) {
-      return true
+      return true;
     } else {
-      return false
+      return false;
     }
   },
   makeQrcode: function (text) {
-    qrcode = new QRCode('canvas', {
+    qrcode = new QRCode("canvas", {
       text: text,
       width: code_w,
       height: code_w,
@@ -73,50 +86,47 @@ Page({
 
   // 获取订单取餐状态
   getOrderPickStatus: function () {
-    let _this = this
+    let _this = this;
     let param = {
-      userCode: wx.getStorageSync('userCode'),
-      orderCode: _this.data.detailInfo.orderCode
-    }
+      userCode: wx.getStorageSync("userCode"),
+      orderCode: _this.data.detailInfo.orderCode,
+    };
     let params = {
       data: param,
-      url: '/order/getPickStatus',
-      method: 'post'
-    }
+      url: "/order/getPickStatus",
+      method: "post",
+    };
     requestModel.request(params, (result) => {
       _this.setData({
         pickStatus: result.pickStatus,
       });
-    })
+    });
   },
   // 修改订单取餐状态 从未取餐改为已取餐
   updateOrderStatus: function () {
-    let _this = this
+    let _this = this;
     let param = {
-      userCode: wx.getStorageSync('userCode'),
-      orderCode: _this.data.detailInfo.orderCode
-    }
+      userCode: wx.getStorageSync("userCode"),
+      orderCode: _this.data.detailInfo.orderCode,
+    };
     let params = {
       data: param,
-      url: '/order/updatePickStatus',
-      method: 'post'
-    }
+      url: "/order/updatePickStatus",
+      method: "post",
+    };
     requestModel.request(params, (result) => {
       wx.showToast({
-        title: '取餐成功',
-        duration: 3000
-      })
-      _this.getOrderPickStatus()
-    })
+        title: "取餐成功",
+        duration: 3000,
+      });
+      _this.getOrderPickStatus();
+    });
   },
-
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-
-  },
+  onReady: function () {},
 
   /**
    * 生命周期函数--监听页面显示
@@ -128,35 +138,25 @@ Page({
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-
-  },
+  onHide: function () {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-
-  },
+  onUnload: function () {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-
-  },
+  onPullDownRefresh: function () {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-
-  },
+  onReachBottom: function () {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-
-  }
-})
+  onShareAppMessage: function () {},
+});
