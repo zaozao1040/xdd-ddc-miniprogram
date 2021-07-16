4fdb0ef59185716106899ce4e1387b705ed82c14
邱宁 <zqiuning@163.com>
2021-07-16 17:45:52 +0800

    修复新人点餐 企业地址不生效问题

--- pages/mine/orgAdminSwcfood/orgAdminSwcfoodDetail.js
+++ pages/mine/orgAdminSwcfood/orgAdminSwcfoodDetail.js
@@ -5,7 +5,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-    info:{}
+    info:{},
+    editStatus:false
 
   },
 
@@ -20,17 +21,13 @@ Page({
     });
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+   clickEdit: function () {
+    this.setData({
+      editStatus:  true
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+   clickCommit: function () {
 
   },
 
