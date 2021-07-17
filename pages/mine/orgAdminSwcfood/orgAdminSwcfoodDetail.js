4fa4f5c3ca2bc40a02b6fa29ea82bdc4d4c74d9b
邱宁 <zqiuning@163.com>
2021-07-17 10:12:51 +0800

    商务餐

--- pages/mine/orgAdminSwcfood/orgAdminSwcfoodDetail.js
+++ pages/mine/orgAdminSwcfood/orgAdminSwcfoodDetail.js
@@ -1,41 +1,98 @@
-// pages/mine/orgAdminSwcfood/orgAdminSwcfoodDetail.js
+import { base } from "../../../comm/public/request";
+let requestModel = new base();
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    userInfo:{},
     info:{},
-    editStatus:false
+    tmp_foodList:[],
+    dialogStatus:false
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+  onLoad: function () {
+    let _this = this;
+    let tmp_tmp_userInfo = wx.getStorageSync("userInfo");
+    if (tmp_tmp_userInfo && tmp_tmp_userInfo.userInfo) {
+      let tmp_userInfo = tmp_tmp_userInfo.userInfo;
+      _this.setData(
+        {
+          userInfo: tmp_userInfo,
+        },
+        () => {
+          _this.loadData();
+        }
+      );
+    }
+  },
+  onLoad: function (options) {    
+    let _this = this;
+    let tmp_tmp_userInfo = wx.getStorageSync("userInfo");
+    if (tmp_tmp_userInfo && tmp_tmp_userInfo.userInfo) {
+      let tmp_userInfo = tmp_tmp_userInfo.userInfo;
+      _this.setData(
+        {
+          userInfo: tmp_userInfo,
+        }
+      );
+    }
     console.log('####### 3 ####### ',getApp().globalData.swcItem);
+    let swcItem = getApp().globalData.swcItem
+    _this.setData({
+      info:swcItem,
+      tmp_foodList:JSON.parse(JSON.stringify(swcItem.foodList))
+    });
+  },
+
+
+
+  clickCommit: function () {
     
     this.setData({
-      info:  getApp().globalData.swcItem
+      dialogStatus:  true
     });
   },
 
-   clickEdit: function () {
-    this.setData({
-      editStatus:  true
+  clickConfirm: function () {
+
+    let _this = this
+
+   let tmp_form = {
+      userCode:_this.data.userInfo.userCode,
+      orderSpecialCode:_this.data.info.orderSpecialCode,
+      foodList:_this.data.tmp_foodList
+    }
+   
+    let params = {
+      data: tmp_form,
+      url: "/business/updateBusinessMeal",
+      method: "post",
+    };
+    requestModel.request(params, () => {
+      wx.showToast({
+        title: '修改成功',
+        icon: 'none',
+        duration: 2000
+      })
+      setTimeout(function () {
+        wx.reLaunch({
+          url: "/pages/mine/orgAdminSwcfood/orgAdminSwcfood",
+        });
+      }, 2000)
     });
   },
 
-   clickCommit: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  clickCounter(e){
+    let {count,type} = e.detail
+    let index = e.currentTarget.dataset.index
+    if(type=='add'){
+      this.data.tmp_foodList[index].foodQuantity++
+    }else if(type=='reduce'){
+      this.data.tmp_foodList[index].foodQuantity--
+    }
   },
 
   /**
