c6cf7ddf54c8edc4fc89fbf0a047b9e62bfa6707
墙上的守夜人 <31274129+zaozao1040@users.noreply.github.com>
2022-10-08 15:42:50 +0800

    提交

--- pages/byc/byc.js
+++ pages/byc/byc.js
@@ -12,6 +12,9 @@ Page({
     userInfo: {},
     qrCode: "",
     errMsg: "",
+    //
+    selectCb: true,
+    selectYe: false,
   },
 
   onLoad: function (options) {
@@ -64,7 +67,86 @@ Page({
       },
     });
   },
-  //  支付
+  // 支付
+  clickZf: function () {
+    let _this = this;
+    if (_this.detailInfo.payType == "NONE") {
+      wx.showToast({
+        title: "暂不支持",
+        duration: 1000,
+      });
+      return;
+    }
+    let obj = {
+      qrCode: _this.data.qrCode,
+      userCode: _this.data.userInfo.userCode,
+      userName: _this.data.userInfo.userName,
+      phoneNumber: _this.data.userInfo.phoneNumber,
+    };
+    let param = {
+      data: obj,
+      method: "post",
+      url: "/order/spareMealOrderScanPay",
+    };
+    requestModel.qqRequest(param, (data) => {
+      if (data.code == 200) {
+        let payData = data.data.payData;
+        wx.requestPayment({
+          timeStamp: payData.timeStamp.toString(),
+          nonceStr: payData.nonceStr,
+          package: payData.packageValue,
+          signType: payData.signType,
+          paySign: payData.paySign,
+          success: function (e) {
+            setTimeout(function () {
+              wx.navigateTo({
+                url: "/pages/byc/bycOrder",
+              });
+            }, 200);
+          },
+          fail: function (e) {
+            console.log("======= fail ======= ");
+            let param = {
+              url:
+                "/order/cancelSpareOrderAndTrade?qrCode=" + _this.data.qrCode,
+              method: "post",
+            };
+            requestModel.qqRequest(param, (data) => {
+              if (data.code == 200) {
+                wx.showToast({
+                  title: "已取消下单",
+                  duration: 1000,
+                });
+              } else {
+                wx.showToast({
+                  title: data.msg,
+                  duration: 1000,
+                });
+              }
+            });
+          },
+        });
+      } else {
+        wx.showModal({
+          title: "提示",
+          content: data.msg,
+          confirmText: "返回上一页",
+          showCancel: false,
+          success(res) {
+            if (res.confirm) {
+              console.log("用户点击确定");
+              wx.reLaunch({
+                url: "/pages/mine/mine",
+              });
+            } else if (res.cancel) {
+              console.log("用户点击取消");
+            }
+          },
+        });
+      }
+    });
+  },
+  //  微信支付  -- 暂时不用
   clickZf: function () {
     let _this = this;
     let obj = {
